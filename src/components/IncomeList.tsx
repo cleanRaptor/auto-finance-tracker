1f322a4f3bd1870576fc4a606e4db0909f33bfc0
cleanRaptor <harrisonderick64@gmail.com>
2024-05-28 11:18:57 +0100

    Create IncomeList.tsx

---
+++ src/components/IncomeList.tsx
@@ -0,0 +1,10 @@
+const IncomeList = () => {
+  return (
+    <div>
+      <h2 className="text-2xl font-semibold">Income</h2>
+      {/* Add income items here */}
+    </div>
+  )
+}
+
+export default IncomeList
