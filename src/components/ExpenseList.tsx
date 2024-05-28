6cf0606abaf5f6465b7842f16518c6a1fa038617
cleanRaptor <harrisonderick64@gmail.com>
2024-05-28 11:19:18 +0100

    Create ExpenseList.tsx

---
+++ src/components/ExpenseList.tsx
@@ -0,0 +1,10 @@
+const ExpenseList = () => {
+  return (
+    <div>
+      <h2 className="text-2xl font-semibold">Expenses</h2>
+      {/* Add expense items here */}
+    </div>
+  )
+}
+
+export default ExpenseList
